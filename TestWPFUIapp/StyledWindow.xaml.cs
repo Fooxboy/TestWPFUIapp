02654d1a677f6d121f8585d98186b0954c92ebb8
Славик Лис <Fooxboy@yandex.ru>
2022-04-01 14:10:47 +0300

    Добавьте файлы проекта.

---
+++ TestWPFUIapp/StyledWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestWPFUIapp
+{
+    /// <summary>
+    /// Логика взаимодействия для StyledWindow.xaml
+    /// </summary>
+    public partial class StyledWindow : Window
+    {
+        public StyledWindow()
+        {
+            InitializeComponent();
+            this.Loaded += StyledWindow_Loaded;
+        }
+
+        private void StyledWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
